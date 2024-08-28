0415ab6367e87de02fc3090342e954ddf5142a05
LVMdS <leo92vinicius@gmail.com>
2024-08-28 20:37:28 -0300

    Exercicio 3

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Ele deve pedir ao jogador um número inteiro entre 0 e 100 e, se a entrada forne
 
 ## Aula 10 - Exercício de revisão 2
 Reescreva seu código para que, em caso de derrota, o jogador seja informado se o número alvo era maior ou menor que o chute
+
+## Aula 10 - Exercício de revisão 3
+Reescreva seu código para que o usuário tenha 5 chutes
---
+++ ex3.py
@@ -0,0 +1,53 @@
+#Usando for
+try:
+    alvo = 12
+    tentativas = 5
+
+    for i in range(tentativas):
+        chute = int(input(f"Tentativa {i+1}/{tentativas}: Digite um número de 0 a 100: "))
+
+        if chute == alvo:
+            print("Parabéns, você acertou!")
+            break
+        else:
+            if chute < alvo:
+                print(f"Que pena, você errou! O número alvo é maior que {chute}.")
+            else:
+                print(f"Que pena, você errou! O número alvo é menor que {chute}.")
+            if i < tentativas - 1:
+                print("Tente novamente.")
+            else:
+                print("Você esgotou todas as suas tentativas.")
+except ValueError:
+    print("O número digitado deve ser um valor inteiro.")
+
+
+#Usando While 
+"""
+try:
+    alvo = 12
+    tentativas = 5
+    chutes_dados = 0
+
+    while chutes_dados < tentativas:
+        chute = int(input(f"Tentativa {chutes_dados+1}/{tentativas}: Digite um número de 0 a 100: "))
+
+        if chute == alvo:
+            print("Parabéns, você acertou!")
+            break
+        else:
+            if chute < alvo:
+                print(f"Que pena, você errou! O número alvo é maior que {chute}.")
+            else:
+                print(f"Que pena, você errou! O número alvo é menor que {chute}.")
+            
+            chutes_dados += 1
+
+            if chutes_dados < tentativas:
+                print("Tente novamente.")
+            else:
+                print("Você esgotou todas as suas tentativas.")
+except ValueError:
+    print("O número digitado deve ser um valor inteiro.")
+    
+"""
