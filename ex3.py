d7fa1572914589af3c6dbc83446101097faa6c16
LVMdS <leo92vinicius@gmail.com>
2024-08-28 21:04:44 -0300

    Exercicio 4

--- ex3.py
+++ ex3.py
@@ -10,14 +10,16 @@ try:
             print("Parabéns, você acertou!")
             break
         else:
-            if chute < alvo:
-                print(f"Que pena, você errou! O número alvo é maior que {chute}.")
-            else:
-                print(f"Que pena, você errou! O número alvo é menor que {chute}.")
+            # Se ainda restam tentativas e não é a última tentativa, dá dicas
             if i < tentativas - 1:
-                print("Tente novamente.")
+                if chute < alvo:
+                    print(f"Que pena, você errou! O número alvo é maior que {chute}.")
+                else:
+                    print(f"Que pena, você errou! O número alvo é menor que {chute}.")
+            if i == tentativas - 1:
+                print(f"Você esgotou todas as suas tentativas. O número alvo era {alvo}.")
             else:
-                print(f"Você esgotou todas as suas tentativas, o numero alvo era {alvo}")
+                print("Tente novamente.")
 except ValueError:
     print("O número digitado deve ser um valor inteiro.")
 
@@ -25,29 +27,31 @@ except ValueError:
 #Usando While 
 """
 try:
-    alvo = 12
-    tentativas = 5
-    chutes_dados = 0
+    alvo = 12 
+    tentativas = 5  
+    chutes_dados = 0  
 
     while chutes_dados < tentativas:
         chute = int(input(f"Tentativa {chutes_dados+1}/{tentativas}: Digite um número de 0 a 100: "))
 
-        if chute == alvo:
+        if chute == alvo: 
             print("Parabéns, você acertou!")
-            break
+            break 
         else:
-            if chute < alvo:
-                print(f"Que pena, você errou! O número alvo é maior que {chute}.")
-            else:
-                print(f"Que pena, você errou! O número alvo é menor que {chute}.")
+            if chutes_dados < tentativas - 1:
+                if chute < alvo:
+                    print(f"Que pena, você errou! O número alvo é maior que {chute}.")
+                else:
+                    print(f"Que pena, você errou! O número alvo é menor que {chute}.")
             
             chutes_dados += 1
 
-            if chutes_dados < tentativas:
-                print("Tente novamente.")
+            if chutes_dados == tentativas:
+                print(f"Você esgotou todas as suas tentativas. O número alvo era {alvo}.")
             else:
-                print("Você esgotou todas as suas tentativas.")
+                print("Tente novamente.")
 except ValueError:
-    print("O número digitado deve ser um valor inteiro.")
+    print("O número digitado deve ser um valor inteiro.") 
+
     
 """
---
+++ ex4.py
@@ -0,0 +1,29 @@
+import random
+
+def adivinhacao():
+    numero_alvo = random.randint(0, 100)
+    tentativas = 3
+    print("Bem-vindo ao jogo de adivinhação! Você tem 3 tentativas.")
+    
+    while tentativas > 0:
+        try:
+            chute = int(input(f"Digite um número inteiro entre 0 e 100 (Tentativas restantes: {tentativas}): "))
+            
+            if chute < 0 or chute > 100:
+                print("Por favor, insira um número válido entre 0 e 100.")
+            elif chute == numero_alvo:
+                print("Parabéns! Você acertou o número!")
+                break
+            elif chute < numero_alvo:
+                print("Tente um número maior.")
+            else:
+                print("Tente um número menor.")
+            
+            tentativas -= 1
+            
+            if tentativas == 0:
+                print(f"Suas tentativas acabaram! O número sorteado era {numero_alvo}.")
+        except ValueError:
+            print("Entrada inválida. Por favor, insira um número inteiro.")
+
+adivinhacao()
